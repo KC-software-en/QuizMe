806d29975aa84a6a426a47035069e615ccf3b77c
KC_software_en <kisha.cairncross@gmail.com>
2024-01-04 09:10:32 +0200

    added education app in settings.py

--- QuizMe/settings.py
+++ QuizMe/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# add education app
 INSTALLED_APPS = [
+    'Education',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
