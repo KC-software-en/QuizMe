c9d90f20747552b06f47a71557f81cae139268b9
Deandre <deandrewright333@gmail.com>
2024-01-05 13:14:22 +0200

    Restuctured comments and tests with docstrings

--- Home/tests/test_urls.py
+++ Home/tests/test_urls.py
@@ -3,19 +3,37 @@ from django.urls import reverse, resolve
 from Home.views import index
 
 # Failing test
+'''
+Create a class to test the Urls.
+''' 
 class TestUrls(SimpleTestCase):
 
-    def test_home_page(self):
-        url = reverse("home") # used to generate a URL based on view name.
-        print("Resolve : ", resolve(url))# Print out the resolve URL to terminal.
+    def setup(self):
+        # no necessary conditions
+        pass
 
-        self.assertEquals(resolve(url).func, 'index') # The assert equals function is used to take the url path and return a object based on the resolver match which is a srting.
+    '''
+        A function to test if the incorrect template is rendered if the user enters the home url path.
+    '''
+    def test_home_page_invalid(self):
+        url = reverse("home") 
+        # used to generate a URL based on view name.
+        print("Resolve : ", resolve(url))
+        # Print out the resolve URL to terminal.
+        
+        self.assertNotEquals(resolve(url).func, 'index') 
+        # The assert equals function is used to take the url path and return a object based on the resolver match which is a srting.
         # The above Testing class will fail because the resolver match should return the index view and not the string 'index'.
 
 # Positive test
-    def test_home_page(self):
+    '''
+        A function to test if the correct template is rendered if the user enters the home url path.
+    '''    
+    def test_home_page_valid(self):
         url = reverse("home")
-        print("Resolve : ", resolve(url)) # Print out the resolve URL to terminal.
+        print("Resolve : ", resolve(url)) 
+        # Print out the resolve URL to terminal.
 
-        self.assertEquals(resolve(url).func, index) # The assert equals function is used to take the url path and return a object based on the resolver match which is a veiw.
+        self.assertEquals(resolve(url).func, index) 
+        # The assert equals function is used to take the url path and return a object based on the resolver match which is a veiw.
         # The above test will pass because the resolver match returns an index view and not the string 'index'.
--- Home/tests/test_veiws.py
+++ Home/tests/test_veiws.py
@@ -1,15 +1,33 @@
 from django.test import TestCase
 from Home.views import index
 
-# Failing test 
+# Failing test
+'''
+Create a class to test the Urls.
+''' 
 class HomeViewTest(TestCase):
-    def test_view_using_template(self):
-        response = self.client.get('/') # Get the ('/') url path as a client.        
-        self.assertTemplateUsed(response, index) # Check if the template can be accessed through the index view.        
+
+    def setup(self):
+        # no necessary conditions
+        pass
+    
+    '''
+        A function to test if the incorrect template is rendered from the view.
+    '''
+    def test_view_using_template_invalid(self):
+        response = self.client.get('/') 
+        # Get the ('/') url path as a client.        
+        self.assertTemplateNotUsed(response, index) 
+        # Check if the template can be accessed through the index view.        
         # The test will fail as the Index view is used as an inccorect template to render then home page it should use the Home page.html string as an assertion templatename.
         
-# Positive test  
-    def test_view_using_template(self):
-        response = self.client.get('/') # Get the ('/') url path as a client.        
-        self.assertTemplateUsed(response, 'index.html')  # Check the Home page.html template can be rendered as a response.
+# Positive test 
+    '''
+        A function to test if the correct template is rendered from the view.
+    ''' 
+    def test_view_using_template_valid(self):
+        response = self.client.get('/') 
+        # Get the ('/') url path as a client.        
+        self.assertTemplateUsed(response, 'index.html')  
+        # Check the Home page.html template can be rendered as a response.
         # The test will pass as the Home page.html string is used as an assertion templatename.
