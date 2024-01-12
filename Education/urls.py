f0ade49cdeca110d546e1eecb7c5d900c6790c6a
Deandre <deandrewright333@gmail.com>
2024-01-12 10:15:58 +0200

    Recieved changes to Education Urls from main.

--- Education/urls.py
+++ Education/urls.py
@@ -1,9 +1,11 @@
 # create urls.py in the directory of the education app
 
-# import path
-# import views
-from operator import index
+# import path to map a URL to a specific view function in Django
+# import include to use other URL patterns from other modules (apps)
 from django.urls import path, include
+
+# import views which contain the functions or classes responsible for handling HTTP requests
+# and returning appropriate responses, rendering web pages.
 from . import views
 
 #############################################################################################
@@ -12,7 +14,11 @@ from . import views
 # add an app_name to set the application namespace
 app_name = 'Education'
 
-# under urlpatterns, paste the code that references the index function in views.py
+# under urlpatterns, paste the code that references the functions in views.py
 urlpatterns = [
-path('', views.index, name='index'),
+    path('', views.index, name='index'),    
+    path('Education/', views.index_edu, name='index_edu'),        
+    path('Education/<int:question_id>/',views.detail, name='detail'),
+    path('Education/<int:question_id>/results/',views.results, name='results'),
+    path('Education/<int:question_id>/vote/',views.vote, name='vote'),
 ]
