52f77231f8b455752090891e4246d4199f7ac064
Deandre <deandrewright333@gmail.com>
2024-01-12 10:00:22 +0200

    Added user_register code and authenticate_user funtion.

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
 # Create your views here.
 def user_login(request):
@@ -7,6 +12,30 @@ def user_login(request):
 def user_logout(request):
     return render(request, 'logout.html')
 
+# This veiw is used to register a new user.
 def user_register(request):
-    return render(request, 'register.html')
-    
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("user_auth:login")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="register.html", context={"register_form":form}) 
+
+#  This veiw is used to authenticate and check if the user already exists.
+def authenticate_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return HttpResponseRedirect(
+            reverse('user_auth:login')
+        )
+    else:
+        login(request, user)
+        return HttpResponseRedirect(
+            reverse('user_auth:logout')
+    )
