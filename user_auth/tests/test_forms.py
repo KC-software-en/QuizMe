20b997f4e1e66b309b6f8e3e47bfcec355c41766
Deandre <deandrewright333@gmail.com>
2024-01-12 11:59:02 +0200

    Added the email field to form tests.

--- user_auth/tests/test_forms.py
+++ user_auth/tests/test_forms.py
@@ -9,13 +9,14 @@ class TestAccountCreationForm(TestCase):
 
         # It creates a dummy user/form and checks if the form is valid and that their is no errors in this case the form is not valid as it is empty.
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 4)
+        self.assertEquals(len(form.errors), 5)
 
 # Positive test if form is valid        
     def test_signup_form(self):
         form = NewUserForm(data={
             'username': 'Jeffrey',
             'first_name': 'Junior',
+            'email': 'testemail@gmail.com',
             'password1': 'Jeffjun2@',
             'password2': 'Jeffjun2@'
         })
@@ -27,6 +28,7 @@ class TestAccountCreationForm(TestCase):
         response = self.client.post(reverse('user_auth:register'), data={
             'username': 'testuser',            
             'first_name': 'Usertest',
+            'email': 'testemail@gmail.com',
             'password1': 'password321@1',
             'password2': 'password321@1'
         })
