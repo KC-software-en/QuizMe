0147f12c523b1b230bf5bc593bd9beb15554bff3
KC_software_en <kisha.cairncross@gmail.com>
2024-01-25 22:22:59 +0200

    tests for TestJsonResponse ok & included in coverage in test_views.py

--- Education/tests/test_views.py
+++ Education/tests/test_views.py
@@ -25,8 +25,11 @@ import json
 # import base class for all Django test cases (for writing tests, including test assertions, database setup and teardown, and other testing infrastructure)
 from django.test import TestCase, RequestFactory, Client
 
-#  import unittest to write tests that are not necessarily tied to Django and can be used in any Python project e.g. get API response from open trivia db
+# import unittest on its own to include in class argument
 import unittest
+
+#  import unittest to write tests that are not necessarily tied to Django and can be used in any Python project e.g. get API response from open trivia db
+from unittest.mock import patch, MagicMock
 
 # import AnonymousUser model - a special type of user object representing an unauthenticated user or a user who is not logged in
 from django.contrib.auth.models import AnonymousUser
@@ -41,6 +44,8 @@ from django.template.response import TemplateResponse
 # import random to shuffle choices rendered in the form
 import random
 
+
+
 #############################################################################################
 #############################################################################################
 # Create your tests here. Use `py manage.py test Education.tests.test_views` to run tests in cmd
@@ -48,70 +53,140 @@ import random
 # next, go to views.py and create the views. Then it should pass
 # run coverage after tests
 
+'''
+Create a class to test the functions that request an API response for categorties on Open Trivia DB.
+'''
 # https://www.django-rest-framework.org/api-guide/testing/#api-test-cases ######## refer back ###
+# use patch class decorator & provide the import path to the requests.get function in your actual codebase
+# patch the external get function within the requests module, which is used in the views.get_json_categories function
+# - to intercept the HTTP request made by the code and control the response during testing
+@patch('Education.views.requests.get')
 class TestJsonResponse(unittest.TestCase):
     def setUp(self):
         pass
+    
+    # test successful response for json categories by calling its function from views 
+    # https://docs.python.org/3.7/library/unittest.mock.html#the-mock-class
+    # mock_get_request is a parameter representing the function that replaces a mock during the test
+    # - represents the requests.get function in the patch decorator, which is called inside the get_json_categories function
+    def test_get_json_categories_ok(self, mock_get_request):  
+        # create an instance of MagicMock & assign it to the response object
+        # simulate a successful HTTP response with a status_code attribute
+        # reflect that the get_json_categories function calls response.json() by
+        # - simulate the expected JSON dictionary by setting a return_value for response.json()
+        mock_response_ok = MagicMock()
+        mock_response_ok.status_code = 200
+        mock_response_ok.json.return_value = {
+            "trivia_categories": [
+                {"id":1, "name":"category1"}, 
+                {"id":2, "name":"category2"}
+                ]
+        }
+        
+        # set the return value for response of mock_get as mock_response
+        mock_get_request.return_value = mock_response_ok
 
-    # https://www.django-rest-framework.org/api-guide/testing/#requestsclient
-    def test_get_json_categories_with_unittest(self):  
-        # use unittest to simulate an external API request    
-        category_lookup = 'https://opentdb.com/api_category.php'        
-        response = requests.get(category_lookup)         
-        if response.status_code == 200:
-            json_response = response.json()            
-                
-        # assert that the HTTP response status code is 200 (OK), indicating a successful request.
-        # assert that API response was successfully received and parsed into a JSON format
+        # call the function in views.py that internally call the requests.get
+        response = views.get_json_categories()
+
+        # assert that API response was successfully received & not None
         # assert that the type of json_response is a dict (dictionary) - ensure a python dictionary
         # - https://docs.python.org/3.7/library/unittest.html?highlight=assertisinstance#unittest.TestCase.assertIsInstance
         # - in Python, everything is an object, and data types are implemented as classes
         # - dict is the class representing dictionaries in Python
         # assert that the key "trivia_categories" is in the dictionary json_response [a in b where assertIn(a,b)]
-        self.assertEqual(response.status_code, 200, msg='Should check that mythology has a successful response.')
-        self.assertIsNotNone(json_response, msg="Should check that an API response was successfully received and parsed into a JSON format.")        
-        self.assertIsInstance(json_response, dict, msg='Should check that json_response is a dictionary.')
-        self.assertIn("trivia_categories", json_response, msg='Should check that the trivia_categories key is present in the dictionary json_response.')
+        self.assertIsNotNone(response, msg='Should check that an API response was successfully received.')
+        self.assertIsInstance(response, dict, msg='Should check that json_response is a dictionary.')
+        self.assertIn('trivia_categories', response, msg='Should check that the trivia_categories key is present in the dictionary json_response.')        
+    
+    # test unsuccessful response for json categories by calling its function from views  
+    # https://docs.python.org/3.7/library/unittest.mock.html#the-mock-class
+    # mock_get_request is a parameter representing the function that replaces a mock during the test
+    # - represents the requests.get function in the patch decorator, which is called inside the get_json_categories function
+    def test_get_json_categories_error(self, mock_get_request):  
+        # create an instance of MagicMock & assign it to the response object
+        # simulate an unsuccessful HTTP response with a status_code attribute        
+        mock_response_error = MagicMock()
+        mock_response_error.status_code = 429
+        mock_response_error.error_message = "Too many requests to the API in a given amount of time ('rate limiting')."
+                
+        # set the return value for response of mock_get_request as mock_response_error
+        mock_get_request.return_value = mock_response_error
 
-    # test specific json category with unittest
-    def test_get_specific_json_category_with_unittest(self):
-        # use unittest to simulate an external API request    
-        mythology_url = f"https://opentdb.com/api.php?amount=50&category=20"
-        response = requests.get(mythology_url)
+        # call the function in views.py that internally call the requests.get
+        response_error = views.get_json_categories()
 
-        # test if there's a successful get request for a specific category
-        if response.status_code == 200:
-            json_response = response.json()
+        # assert that API response was unsuccessful & None        
+        self.assertIsNone(response_error, msg='Should check that the categories API response was unsuccessful & returns None.')        
 
-        # test if there's an unsuccessful get request for a specific category
-        else:
-            error_message = f"Unable to retrieve the specific category - Status code:{response.status_code}"
-            print(error_message)
-            json_response = None
+    # test successful response for specific json categories by calling its function from views  
+    # mock_get_request is an argument representing the mocked version of the requests.get function specified in the @patch decorator, 
+    # - used within the get_specific_json_category function.
+    def test_get_specific_json_category_success(self, mock_get_request):
+        # set up mock for a successful response
+        # create a mock object by instantiating a MagicMock class & assigning it to the response object
+        # - it will simulate an HTTP response object
+        # - MagicMock allows you to access and set attributes, call methods, etc, without explicitly defining them
+        # simulate a successful HTTP response with a status_code attribute
+        # reflect that the get_specific_json_category function calls response.json() by
+        # - simulate the expected JSON dictionary by setting a return_value for response.json()
+        mock_ok_response = MagicMock()
+        mock_ok_response.status_code = 200
+        mock_ok_response.json.return_value = {
+            "results":[
+                {"type":"type1",
+                 "difficulty":"level",
+                 "category":"category1",
+                 "question":"question1",
+                 "correct_answer":"correct_answer1",
+                 "incorrect_answers":[
+                     "incorrect_answer",
+                     "incorrect_answer2",
+                     "incorrect_answer3",                     
+                 ]
+                 }
+            ]
+        }
 
-        # assert that get request renders succesfully
-        # 
-        self.assertEqual(response.status_code, 200, msg='Should check that mythology has a successful response.')
-        self.assertIsNotNone(json_response, msg="Should check that the mythology API response was successfully received and parsed into a JSON format.")        
-        self.assertIsInstance(json_response, dict, msg='Should check that the json_response is a dictionary.')
-        self.assertIn("results", json_response, msg='Should check that the results key is present in the dictionary json_response.')
+        # set the return value for response of mock_get as mock_response
+        mock_get_request.return_value = mock_ok_response
 
-    # test json categories by calling its function from views 
-    def test_get_json_categories_call(self):      
-        response = views.get_json_categories()
-        self.assertIsNotNone(response)
-        self.assertIsInstance(response, dict)
-        self.assertIn('trivia_categories', response)
+        # call the function in views.py that internally call the requests.get
+        response_ok = views.get_specific_json_category(quantity=50, category=20)
+        
+        # assert that API response was successfully received & not None
+        # assert that the type of json_response is a dict (dictionary) - ensure a python dictionary                
+        # - dict is the class representing dictionaries in Python
+        # assert that the key "results" is in the dictionary json_response [a in b where assertIn(a,b)]
+        self.assertIsNotNone(response_ok, msg='Should check that an API response was successfully received.')
+        self.assertIsInstance(response_ok, dict, msg='Should check that json_response is a dictionary.')
+        self.assertIn('results', response_ok, msg='Should check that the trivia_categories key is present in the dictionary json_response.')        
 
-    # test json specific categories by calling its function from views  
-    def test_get_specific_json_category_call(self):
-        response = views.get_specific_json_category(quantity=5, category=20)
-        self.assertIsNotNone(response)
-        self.assertIsInstance(response, dict)
-        self.assertIn('results', response)        
+    # test error response for specific json categories by calling its function from views  
+    # mock_get_request is an argument representing the mocked version of the requests.get function specified in the @patch decorator, 
+    # - used within the get_specific_json_category function.
+    def test_get_specific_json_category_error(self, mock_get_request):
+        # set up mock for an unsuccessful response
+        # create a mock object by instantiating a MagicMock class & assigning it to the response object
+        # - it will simulate an HTTP response object
+        # - MagicMock allows you to access and set attributes, call methods, etc, without explicitly defining them
+        # simulate an unsuccessful HTTP response with a status_code attribute e.g. 429 for rate limit exceeded        
+        # simulate an error message for the mock test
+        mock_error_response = MagicMock()
+        mock_error_response.status_code = 429
+        mock_error_response.error_message = "Too many requests to the API in a given amount of time ('rate limiting')."
+
+        # set the return value for response of mock_get as mock_response
+        mock_get_request.return_value = mock_error_response
+
+        # call the function in views.py that internally call the requests.get
+        response_error = views.get_specific_json_category(quantity=50, category=20)
+        
+        # assert that API response was unsuccessful & returns None        
+        self.assertIsNone(response_error, msg='Should check that an API response was unsuccessful & returns None.')        
 
 '''
-Create ...
+Create a class to test the views rendered in Education.
 '''
 #
 class TestEducationViews(TestCase):
@@ -193,7 +268,7 @@ class TestGetQuestionsAndChoices(TestCase):
         request = self.factory.get('/path/to/your/view/50/20/')
         
         # Call the view function with the GET request
-        response = get_questions_and_choices(request, quantity=50, category=20)
+        response = views.get_questions_and_choices(request, quantity=50, category=20)
 
         # Assert that the response has a status code of 200
         self.assertEqual(response.status_code, 200)
@@ -205,11 +280,11 @@ class TestGetQuestionsAndChoices(TestCase):
         request = self.factory.get('/path/to/your/view/50/20/')
         
         # Mock the get_specific_json_category function to return a response without questions
-        with patch('your_app.views.get_specific_json_category') as mock_get_specific_json:
+        with patch('Education.views.get_specific_json_category') as mock_get_specific_json:
             mock_get_specific_json.return_value = {'results': []}
 
             # Call the view function with the GET request
-            response = get_questions_and_choices(request, quantity=50, category=20)
+            response = views.get_questions_and_choices(request, quantity=50, category=20)
 
         # Assert that the response has a status code of 200
         self.assertEqual(response.status_code, 200)
@@ -237,3 +312,48 @@ possible assertions
         self.assertIsNone(response.context['error_message'])
 '''
 
+#removed these tests after implementing mock, patch
+'''
+# https://www.django-rest-framework.org/api-guide/testing/#requestsclient
+    def test_get_json_categories_with_unittest(self):  
+        # use unittest to simulate an external API request    
+        category_lookup = 'https://opentdb.com/api_category.php'        
+        response = requests.get(category_lookup)         
+        if response.status_code == 200:
+            json_response = response.json()                    
+                
+        # assert that the HTTP response status code is 200 (OK), indicating a successful request.
+        # assert that API response was successfully received and parsed into a JSON format
+        # assert that the type of json_response is a dict (dictionary) - ensure a python dictionary
+        # - https://docs.python.org/3.7/library/unittest.html?highlight=assertisinstance#unittest.TestCase.assertIsInstance
+        # - in Python, everything is an object, and data types are implemented as classes
+        # - dict is the class representing dictionaries in Python
+        # assert that the key "trivia_categories" is in the dictionary json_response [a in b where assertIn(a,b)]
+        self.assertEqual(response.status_code, 200, msg='Should check that mythology has a successful response.')
+        self.assertIsNotNone(json_response, msg="Should check that an API response was successfully received and parsed into a JSON format.")        
+        self.assertIsInstance(json_response, dict, msg='Should check that json_response is a dictionary.')
+        self.assertIn("trivia_categories", json_response, msg='Should check that the trivia_categories key is present in the dictionary json_response.')
+
+    # test specific json category with unittest
+    def test_get_specific_json_category_with_unittest(self):
+        # use unittest to simulate an external API request    
+        mythology_url = f"https://opentdb.com/api.php?amount=50&category=20"
+        response = requests.get(mythology_url)
+
+        # test if there's a successful get request for a specific category
+        if response.status_code == 200:
+            json_response = response.json()
+
+        # test if there's an unsuccessful get request for a specific category
+        else:
+            error_message = f"Unable to retrieve the specific category - Status code:{response.status_code}"
+            print(error_message)
+            json_response = None
+
+        # assert that get request renders succesfully
+        # 
+        self.assertEqual(response.status_code, 200, msg='Should check that mythology has a successful response.')
+        self.assertIsNotNone(json_response, msg="Should check that the mythology API response was successfully received and parsed into a JSON format.")        
+        self.assertIsInstance(json_response, dict, msg='Should check that the json_response is a dictionary.')
+        self.assertIn("results", json_response, msg='Should check that the results key is present in the dictionary json_response.')
+'''
