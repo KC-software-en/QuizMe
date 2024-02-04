b6b8bf7b86648fe302ca7a8c6c6b4ad2febf0715
KC_software_en <kisha.cairncross@gmail.com>
2024-02-04 15:41:16 +0200

    registered Quiz model in admin.py

--- Education/admin.py
+++ Education/admin.py
@@ -1,14 +1,13 @@
 # import admin to customise the admin interface, register models, and define admin views and actions
 from django.contrib import admin
 
+##############################################################################################
+
 # import Question & Choice classes from models.py
-from .models import Question
-from .models import Choice
+from .models import Quiz
 
 # Register your models here.
 # Register the Question class, so that it will be available when we login
 # to the admin page of our site.
 # Register the Choice class
-admin.site.register(Question)
-admin.site.register(Choice)
-
+admin.site.register(Quiz)
