5806067a8053bf040f75827168f605a981465938
Deandre <deandrewright333@gmail.com>
2024-02-16 10:54:02 +0200

    Added the History model.

--- Education/models.py
+++ Education/models.py
@@ -30,3 +30,24 @@ class Mythology(models.Model):
     # return question
     def __str__(self):
         return self.question
+    
+
+class History(models.Model):
+    '''
+    Create a model for the Science and nature questions in the education quiz.
+    '''
+    # set variables that represent question, choices & correct_answer in the quiz
+    # use TextField to store the questions
+    question = models.TextField()    
+    
+    # use TextField to store the list of choices 
+    # set default as an empty list and allow it to be blank.
+    choices = models.TextField(default = list, blank = True)
+
+    # use TextField to store the correct answer
+    correct_answer = models.TextField()
+
+    # define a __str__ method for human-readable output
+    # return question
+    def __str__(self):
+        return self.question    
