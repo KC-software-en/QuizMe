d8829ca49d9e7549739704e3ff467769c6f61359
Deandre <deandrewright333@gmail.com>
2024-02-16 10:53:13 +0200

    Registered the History model in admin.py.

--- Education/admin.py
+++ Education/admin.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 ##############################################################################################
 
 # import Question & Choice classes from models.py
-from .models import Mythology
+from .models import Mythology, History
 
 # Register your models here.
 # Register the Question class, so that it will be available when we login
 # to the admin page of our site.
 # Register the Choice class
 admin.site.register(Mythology)
+
+# Register the Science and nature model to the admin site
+admin.site.register(History)
