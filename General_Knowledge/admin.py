568875f49b21e5728606d3386f5348b7c12922a3
KC_software_en <kisha.cairncross@gmail.com>
2024-03-18 22:55:24 +0200

    registered Categories & Subcategories in General_Knowledge/admin.py

--- General_Knowledge/admin.py
+++ General_Knowledge/admin.py
@@ -2,10 +2,16 @@
 from django.contrib import admin
 
 # import classes from models.py
-from .models import General_Knowledge
+from .models import Categories, Subcategories, General_Knowledge
 
 ##############################################################################################
 
 # Register your models here.
+# Register the Categories class, so that it will be available on the admin site
+admin.site.register(Categories)
+
+# Register the Subcategories class, so that it will be available on the admin site
+admin.site.register(Subcategories)
+
 # Register the General_Knowledge model to the admin site
 admin.site.register(General_Knowledge)
