dba29cfa5c2ddda2e1a8bfba6732ab90784d6465
Deandre <deandrewright333@gmail.com>
2024-03-28 07:44:29 +0200

    Assigned enviroment variables for the database and emaiil backend.

--- QuizMe/settings.py
+++ QuizMe/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+# Call the enviroment variables.
+from dotenv import load_dotenv
+
+# Load all the enviroment variables.
+load_dotenv()
 
 # import os to access environment variables and retrieve sensitive information without hardcoding it into the settings file
 # i.e. secret key
@@ -123,11 +128,11 @@ elif DEBUG and (DATABASE == 2):
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Q_ME123@1',
-        'HOST': 'localhost',
-        'POST': '5432'
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'POST': os.environ.get('DATABASE_POST')
     }
 
 }
@@ -167,12 +172,12 @@ USE_L10N = True
 USE_TZ = True
 
 # Django email Backend.
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'QuizMe2024@gmail.com'
-EMAIL_HOST_PASSWORD = 'pjxp atrk kbgu aoaq'
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
