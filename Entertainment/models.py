176bc1d0d49013d15bdfdca228edd7f588f592d0
KC_software_en <kisha.cairncross@gmail.com>
2024-09-21 22:31:04 +0200

    removed get() queries from within models that have relationships & set default values in Entertainment/models.py

--- Entertainment/models.py
+++ Entertainment/models.py
@@ -1,13 +1,13 @@
+# import models
 from django.db import models
 
 ###################################################################################
 ###################################################################################
 
 # Create your models here.
+
 # define a model for categories in the QuizMe project
 class Categories(models.Model):
-    """Create a model for the quiz categories in QuizMe.
-    """
     # define a field to store the category name as text
     # define a field to store the description of the category as text
     category = models.TextField()
@@ -23,14 +23,8 @@ class Categories(models.Model):
         """
         return self.category
     
-
-# define a model for subcategories in the Entertainment app
+# define a model for the quiz subcategories in Entertainment
 class Subcategories(models.Model):
-    """Create a model for the quiz subcategories in Entertainment.
-    """
-    # retrieve the category object representing 'Entertainment' from the Categories model
-    en_category_obj = Categories.objects.get(category='Entertainment')
-
     # use database foreign keys to indicate relationships between categories and subcategories
     # - a ForeignKey field to show this many-to-one relationship
     # set a default category to the primary key (pk) of the 'Entertainment' category object
@@ -41,7 +35,7 @@ class Subcategories(models.Model):
     # https://docs.djangoproject.com/en/3.2/ref/models/fields/#default
     # - according to doc, for fields like ForeignKey that map to model instances, 
     # - defaults should be the value of the field they reference (pk unless to_field is set) instead of model instances
-    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default=en_category_obj.pk)    
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default='Entertainment')    
 
     # define a field to store the subcategory name as text
     subcategory = models.TextField()
@@ -64,22 +58,15 @@ class Subcategories(models.Model):
 # -each entry in a SQL table represents a single object, this can be converted to a class instance in Python.  
 # create a Music class that inherits from django.db.models.Model
 class Music(models.Model): 
-    """Create a Music model for the subcategory in the entertainment quiz.
-    """ 
-    # retrieve the category object representing 'Education' from the Categories model
-    en_category_obj = Categories.objects.get(category='Entertainment')
-    # retrieve the subcategory object representing 'Music' from the Subcategories model
-    music_subcategory_obj = Subcategories.objects.get(subcategory='Music')
-
     # use database foreign keys to indicate relationships between Music and categories    
     # use database foreign keys to indicate relationships between Music and subcategories
-    # - a ForeignKey field to show this many-to-one relationship
-    # set a default category to the primary key (pk) of the 'en_category_obj' object
-    # set the desired default subcategory to the primary key (pk) of the 'music_subcategory_obj'
+    # - a ForeignKey field to show this many-to-one relationship    
     # ‘on_delete’ parameter allows one to define the behaviour when the referenced object is deleted, 
     # - providing further control over the default value
-    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default=en_category_obj.pk)   
-    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default=music_subcategory_obj.pk)       
+    # set a default category to Entertainment
+    # set the default subcategory to Music
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default='Entertainment')   
+    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default='Music')       
 
     # set variables that represent question, choices & correct_answer in the quiz
     # use TextField to store the questions
@@ -104,22 +91,15 @@ class Music(models.Model):
 
 # create a Film class that inherits from django.db.models.Model
 class Film(models.Model):   
-    """Create a Film model for the subcategory in the entertainment quiz.
-    """ 
-    # retrieve the category object representing 'Entertainment' from the Categories model
-    en_category_obj = Categories.objects.get(category='Entertainment')
-    # retrieve the subcategory object representing 'Film' from the Subcategories model
-    film_subcategory_obj = Subcategories.objects.get(subcategory='Film')
-
     # use database foreign keys to indicate relationships between Film and categories    
     # use database foreign keys to indicate relationships between Film and subcategories
     # - a ForeignKey field to show this many-to-one relationship
-    # set a default category to the primary key (pk) of the 'en_category_obj' object
-    # set the desired default subcategory to the primary key (pk) of the 'film_subcategory_obj'
     # ‘on_delete’ parameter allows one to define the behaviour when the referenced object is deleted, 
     # - providing further control over the default value
-    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default=en_category_obj.pk)   
-    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default=film_subcategory_obj.pk)       
+    # set a default category to Entertainment
+    # set the default subcategory to Film    
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default='Entertainment')   
+    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default='Film')       
 
     # set variables that represent question, choices & correct_answer in the quiz
     # use TextField to store the questions
@@ -142,23 +122,17 @@ class Film(models.Model):
         """
         return self.question
 
+# create a Video_Games class that inherits from django.db.models.Model
 class Video_Games(models.Model):
-    """Create a model for the Science and nature questions in the entertainment quiz.
-    """ 
-    # retrieve the category object representing 'Entertainment' from the Categories model
-    en_category_obj = Categories.objects.get(category='Entertainment')
-    # retrieve the subcategory object representing 'Video Games' from the Subcategories model
-    games_subcategory_obj = Subcategories.objects.get(subcategory='Video Games')
-
     # use database foreign keys to indicate relationships between Video Games and categories    
     # use database foreign keys to indicate relationships between Video Games and subcategories
-    # - a ForeignKey field to show this many-to-one relationship
-    # set a default category to the primary key (pk) of the 'en_category_obj' object
-    # set the desired default subcategory to the primary key (pk) of the 'games_subcategory_obj'
+    # - a ForeignKey field to show this many-to-one relationship    
     # ‘on_delete’ parameter allows one to define the behaviour when the referenced object is deleted, 
     # - providing further control over the default value
-    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default=en_category_obj.pk)   
-    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default=games_subcategory_obj.pk)       
+    # set a default category to Entertainment
+    # set the default subcategory to Video Games
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, default='Entertainment')   
+    subcategory = models.ForeignKey(Subcategories, on_delete=models.CASCADE, default='Video Games')       
     
     # set variables that represent question, choices & correct_answer in the quiz
     # use TextField to store the questions
